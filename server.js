88cc249674e9777dcc0ab5ebd8f4ab243a690642
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-11-01 10:54:15 -0400

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
-app.use(morgan('dev'));
+
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 require('dotenv').config();
@@ -49,8 +52,17 @@ app.use(function validateBearerToken(req, res, next) {
 
 app.get('/movie', handleGetMovie);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
 });
